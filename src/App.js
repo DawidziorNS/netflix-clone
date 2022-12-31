30082d4d26e43504082f4633b31677cee30e8901
dawid <dawidjasinski@my.com>
2022-12-31 16:58:33 +0100

    main: Implement LoginScreen.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './App.css';
 import HomeScreen from "./screens/HomeScreen";
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
+    const user = null
+
     return (
         <div className="app">
             <Router>
-                <Routes>
-                    <Route exact path="/" element={<HomeScreen />}/>
-                </Routes>
+                {
+                    !user ? (<LoginScreen />) : (
+                        <Routes>
+                            <Route exact path="/" element={<HomeScreen />}/>
+                        </Routes>
+                    )
+                }
             </Router>
         </div>
     );
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,9 @@
+import './SignupScreen.css'
+
+const SignupScreen = () => {
+    return <div className="signupScreen">
+
+    </div>
+}
+
+export default SignupScreen
